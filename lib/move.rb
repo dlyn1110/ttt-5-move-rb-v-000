770dda3d5a934acc5abe0ee2f52b24224f305f6d
Dani Lyn <dlyn1110@gmail.com>
2018-03-23 12:22:19 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# codef input_to_index(user_input)
+
+def input_to_index(user_input)
+  user_input = "1".to_i
+end

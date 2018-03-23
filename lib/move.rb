6733a378acc50a24a814c2e5301960b474c0ed9e
Dani Lyn <dlyn1110@gmail.com>
2018-03-23 12:44:10 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# codef input_to_index(user_input)
+# code input_to_index(user_input)
 
 def input_to_index(input)
   input = input.to_i
@@ -14,5 +14,5 @@ def input_to_index(input)
 end
 
 def move [board, index, value]
-  board[index] = value 
+  board[index] = value
 end
